630b6e6c6a2b2a56a8f1bb17b461bf808881836f
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-14 11:49:34 +0200

    JAVA OOP  Constructor, toString

--- src/main/java/example/CircleClass/Circle.java
+++ src/main/java/example/CircleClass/Circle.java
@@ -1,4 +1,32 @@
 package example.CircleClass;
+/*Circle class
+Implement the  Circle  class. It should extend the  Shape  class. Implementation
+should meet the below criteria:
+information about the radius value
+non-arguments constructor which will set: the  color  eld value to  unknown ,
+the  isFilled  eld value to  false  and the  radius  eld value to  1
+three-arguments constructor:  color ,  isFilled ,  radius
+getter  methods which will be responsible for returning the  radius  value
+setter  methods which will be responsible for setting the  radius  value
+getArea  method which will be responsible for calculating the surface area
+getPerimeter  method which will be responsible for calculating the circuit
+toString  method which should return the following information:  Circle
+with radius=? which is a subclass off y , where ? is a placeholder for
+the radius value and the  y  info should be a result of the  toString
+execution from the base class*/
 
-public class Circle {
+public class Circle extends Shape{
+    private double radius;
+
+    // Non-arguments constructor
+    public Circle(double radius){
+        super();
+        this.radius = 1;
+    }
+
+    // Three-arguments constructor
+    public Circle(double radius, String color){
+        super(color, false);
+        this.radius = radius;
+    }
 }
