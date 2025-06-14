125fc27cf2f9b417bcddbc31adb8624de56850a1
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-14 10:31:51 +0200

    JAVA OOP

---
+++ src/main/java/example/Person.java
@@ -0,0 +1,15 @@
+package example;
+/*Person class
+Implement the  Person  abstract class. It should contain:
+two String elds:  name ,  address
+non-arguments constructor which will set  name ,  address  elds as empty
+strings
+two-arguments constructor:  String name ,  String address
+getter  methods which will be responsible for returning  name ,  address
+elds values
+setter  methods which will be responsible for setting  name ,  address  elds
+values
+toString  method which should return string in the following format:  ?->? ,
+where  ?  is the name and adress value accordingly*/
+public class Person {
+}
