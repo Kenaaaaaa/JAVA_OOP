4412090057597c111937215085d4c28afea84ea8
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-15 12:00:12 +0200

    JAVA OOP  Constructor, toString

--- src/main/java/example/Generics/Generics.java
+++ src/main/java/example/Generics/Generics.java
@@ -2,7 +2,21 @@ package example.Generics;
 
 public class Generics {
     public static void main(String[] args) {
-
+        Integer[] integers = new Integer[10];
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = i * 10;
+        }
+        printArray(integers); // Print Integer array
+        String[] strings = new String[10];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = "String " + i;
+        }
+        printArray(strings); // Print String array
+        Double[] doubles = new Double[10];
+        for (int i = 0; i < doubles.length; i++) {
+            doubles[i] = i * 1.5;
+        }
+        printArray(doubles); // Print Double array
 
         //Generic method to print arrays of any type
 
