fa849bf716ab1a46e5fa7e197bdcdbedc941c7aa
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-23 21:09:52 +0200

    polymorphysm

--- Polymorphism/src/main/java/VodPlayer/Main.java
+++ Polymorphism/src/main/java/VodPlayer/Main.java
@@ -1,4 +1,16 @@
 package VodPlayer;
 
 public class Main {
+    public static void main(String[] args) {
+        final String player=args[0];
+        VodPlayer vodPlayer=null;
+        if(player.equalsIgnoreCase("Netflix")){
+            vodPlayer=new NetflixPlayer();
+        }else if(player.equalsIgnoreCase("Giant")){
+            vodPlayer=new HBOGoPlayer();
+        }else if(player.equalsIgnoreCase("Microsoft")){
+            vodPlayer=new DefaultPlayer();
+        }
+
+    }
 }
